592f80c6daeaa3f5b04ae5d51d9a22b1dd0e379d
Andreas Sandberg <andreas@sandberg.pp.se>
2020-05-08 12:33:36 +0100

    drivers: clock_control: stm32: Enable SYSCFG on STM32L0
    The entropy driver needs to use the SYSCFG block to control VREF on
    the STM32L0. Clock the block if the entropy driver has been enabled.
    
    Signed-off-by: Andreas Sandberg <andreas@sandberg.pp.se>

--- drivers/clock_control/clock_stm32l0_l1.c
+++ drivers/clock_control/clock_stm32l0_l1.c
@@ -38,7 +38,8 @@ void config_pll_init(LL_UTILS_PLLInitTypeDef *pllinit)
  */
 void config_enable_default_clocks(void)
 {
-#if defined(CONFIG_EXTI_STM32) || defined(CONFIG_USB_DC_STM32)
+#if defined(CONFIG_EXTI_STM32) || defined(CONFIG_USB_DC_STM32) || \
+	(CONFIG_SOC_SERIES_STM32L0X && ENTROPY_STM32_RNG)
 	/* Enable System Configuration Controller clock. */
 	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
 #endif
