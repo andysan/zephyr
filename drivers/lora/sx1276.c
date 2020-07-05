50795417cba35f97ac131570eb8fa0dbf25c2cfd
Manivannan Sadhasivam <mani@kernel.org>
2020-07-05 21:37:37 +0530

    drivers: lora: Add missing function definitions & callbacks for LoRaWAN
    For preparing the radio drivers (specifically SX1276) for LoRaWAN
    support, let's add missing function definitions and callbacks required
    by the stack.
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

--- drivers/lora/sx1276.c
+++ drivers/lora/sx1276.c
@@ -151,6 +151,11 @@ bool SX1276CheckRfFrequency(uint32_t frequency)
 	return true;
 }
 
+uint32_t SX1276GetBoardTcxoWakeupTime(void)
+{
+	return TCXO_POWER_STARTUP_DELAY_MS;
+}
+
 static inline void sx1276_antenna_enable(int val)
 {
 #if DT_INST_NODE_HAS_PROP(0, antenna_enable_gpios)
@@ -443,6 +448,7 @@ const struct Radio_s Radio = {
 	.Random = SX1276Random,
 	.SetRxConfig = SX1276SetRxConfig,
 	.SetTxConfig = SX1276SetTxConfig,
+	.TimeOnAir = SX1276GetTimeOnAir,
 	.Send = SX1276Send,
 	.Sleep = SX1276SetSleep,
 	.Standby = SX1276SetStby,
@@ -452,6 +458,8 @@ const struct Radio_s Radio = {
 	.WriteBuffer = SX1276WriteBuffer,
 	.ReadBuffer = SX1276ReadBuffer,
 	.SetMaxPayloadLength = SX1276SetMaxPayloadLength,
+	.SetPublicNetwork = SX1276SetPublicNetwork,
+	.GetWakeupTime = SX1276GetWakeupTime,
 	.IrqProcess = NULL,
 	.RxBoosted = NULL,
 	.SetRxDutyCycle = NULL,
